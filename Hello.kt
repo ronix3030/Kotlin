01aeadc380f766d247a18c0c1169c0c4e4693ef6
ronix3030 <ronald3019@hotmail.com>
2024-03-05 11:11:06 -0500

    Variables

---
+++ Hello.kt
@@ -0,0 +1,53 @@
+fun main() {
+   println("Hola")
+   println("Hola")
+   println("Hola")
+   println("Hola")
+
+    // Declaración de Strings
+   val hola1: String = "Hola Mundo"
+   val hola2: String = "Hola Mundo 2"
+
+
+   println(hola1)
+   println(hola2)
+
+    // Concatenación de Strings
+   val nom = "ron"
+    val saludar = "Hola, $nom" // Recuerda que aquí se usa la interpolación de strings
+
+   println(saludar)
+    // Booleanos
+   val verdad: Boolean = true
+   val falsedad: Boolean = false 
+
+   println(verdad)
+   println(falsedad)
+
+    // Declaración de Enteros
+   val entero: Int = 42
+   val largo: Long = 93187293
+   val max5: Short = 12345
+   val decimales: Double = 5218.874
+
+   println(entero)
+   println(largo)
+   println(max5)
+   println(decimales)
+
+
+   //Operaciones Aritméticas
+
+   val a=10
+   val b=70
+
+   val sumar=a+b 
+   val rest=b-a 
+   val multi=a*b 
+   val divi=b/a 
+
+   println(sumar)
+   println(rest)
+   println(multi)
+   println(divi)
+}
